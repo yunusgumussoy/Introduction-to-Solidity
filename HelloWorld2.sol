f099f2767cde694ea2d08cfc7e35ef0e0954985f
Yunus GÜMÜŞSOY <34836874+yunusgumussoy@users.noreply.github.com>
2021-12-18 11:21:08 +0300

    Update HelloWorld2.sol

--- HelloWorld2.sol
+++ HelloWorld2.sol
@@ -1,3 +1,6 @@
+// SPDX-License-Identifier: MIT
+// Yunus GUMUSSOY - The First Step of Being a Blockchain Developer
+
 pragma solidity ^0.4.8;
 contract HelloWorld {
     uint256 counter = 0;
@@ -10,12 +13,16 @@ contract HelloWorld {
         counter--;
     }
 
-    function getCounter() view returns(uint256){
+    // function getCounter() view returns(uint256){ / public or private should be added to determine visibility
+    // function getCounter() private view returns(uint256){ / private means they can't be accessed or modified from other smart contracts
+    function getCounter() public view returns(uint256){
         return counter;
     }
 
 
     function killme() public {
-        suicide(msg.sender);
+        // suicide is replaced by selfdestruct
+        // suicide(msg.sender);
+        selfdestruct(msg.sender);
     }
 }
